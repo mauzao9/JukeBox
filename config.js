e864cdc854c0d72a62d1150bd4d98f45133503db
mauzao9 <mr.max.zyppy@gmail.com>
2016-11-21 20:52:12 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    botApiKey: 'slackbotApiKey',
-    youtubeApiKey: 'youtubeApiKey',
-    homeChannel: 'main bot`s channelId',
+    botApiKey: 'xoxb-107567328069-pJBQ0pMaPXNnabD5TSX8XpXw',
+    youtubeApiKey: 'AIzaSyD3rDjvZVfZZJVxzN76QMnGwuMD6A32AYI',
+    homeChannel: '-lobby-',
     admins: [],
 };
